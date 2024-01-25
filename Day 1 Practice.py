a06bc206183b0e30bfb675b3d3ba6eb75a1abbd5
Clay <clayp31827@outlook.com>
2024-01-24 23:34:17 -0500

    First commit for Day 1

---
+++ Day 1 Practice.py
@@ -0,0 +1,47 @@
+#Intro
+#first_name = "Fred"
+#print(first_name)
+
+#my_first_var = 3
+
+#1.1
+text = input("Enter words:")
+print(text)
+
+#1.2
+number = input("Enter a number:")
+number2 = int(number)
+print(number2 + 1)
+
+#1.3
+number_1 = input("Enter a number:")
+number_2 = float(number_1)
+print(number_2 + 0.5)
+
+#1.4
+num1 = input("Enter a number:")
+num_1 = float(num1)
+num2 = input("Enter a number:")
+num_2 = float(num2)
+print(num_1 + num_2)
+
+#1.5
+value = input("Enter a number:")
+numb1 = float(value)
+value2 = input("Enter a number:")
+numb2 = float(value2)
+print(numb1 * numb2)
+
+#1.6
+val = input("Enter a number:")
+n1 = int(val)
+val2 = input("Enter a number:")
+n2 = int(val2)
+print(n1 / n2)
+
+#1.7
+
+user_input = input("Enter a boolean:")
+right = user_input == "True"
+print("You entered:", right)
+print("The opposite of what you entered is:", not right)
